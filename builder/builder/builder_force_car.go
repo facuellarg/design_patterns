a8bc1a7ac17b9ef5597e159d8f7edb1efac40371
facuellarg <facuellarg@unal.edu.co>
2021-11-10 19:27:37 -0500

    builders implementation

---
+++ builder/builder/builder_force_car.go
@@ -0,0 +1,24 @@
+package builder
+
+type builderForceCar struct {
+	car Car
+}
+
+func NewBuilderForceCar() *builderForceCar {
+	return &builderForceCar{Car{}}
+}
+
+func (bf *builderForceCar) Build() Car {
+	buildedCar := bf.car
+	bf.Reset()
+	return buildedCar
+}
+func (bf *builderForceCar) Reset() {
+	bf.car = Car{}
+}
+func (bf *builderForceCar) BuildMotor() {
+	bf.car.SetMotor(Motor{200})
+}
+func (bf *builderForceCar) BuildChair() {
+	bf.car.SetChair(Chair{})
+}
